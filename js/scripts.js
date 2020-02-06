ba66eda2798b8b15e5276a53e937bf33b9fd70ce
ox-mariia <60494463+ox-mariia@users.noreply.github.com>
2020-02-06 15:17:16 -0800

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,123 @@
+// STEP 1
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+
+
+// STEP 2
+17 //numeric literal expression
+"Hello, World!" //string literal expression
+false //boolean literal expression
+null //null literal expression
+
+// STEP 3 
+let x = 45 * (17 - 12); // complex / variable expression
+let yx = x * (150 * 0.25); // complex / variable expression
+
+// STEP 4 
+let sFirstName;
+let sLastName;
+let sAddress;
+let sCity;
+let sState;
+let nZipCode;
+let nYourAge;
+let sRefferalSource;
+let bMayWeContactYou;
+
+// STEP 5
+let sFirstName = "Mariia", sLastName = "Okhota";
+
+let sCity;
+sCity = "Dnipro";
+
+let sState = "California";
+let nZipCode = 92115;
+
+// STEP 6
+let ticketPrice = 170;
+window.console.log("The Hawaii ticket price is " + ticketPrice + " dollars.");
+
+// STEP_7
+let x = false;
+window.console.log("Are we alone in the Universe? " + x);
+
+let y = 5
+window.console.log(y + false);
+
+// STEP_8  fixed version
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+
+// STEP_9
+let a = null;
+window.console.log(a);
+let b;
+window.console.log(Blob);
+
+// STEP_10
+window.console.log(typeof "Mariia");//string
+window.console.log(typeof 17);//number
+window.console.log(typeof true);//boolean
+window.console.log(typeof [1, 2, 3]);//object
+window.console.log(typeof a);//undefined
+
+// STEP_11
+alert("Hello " + "Mariia Okhota" + ", Welcome to the JavaScript class!");
+
+// STEP_12
+let name = "Mariia Okhota";
+alert("Hello " + name + ", Welcome to the JavaScript class!");
+
+// STEP_13
+let course = "JavaScript";
+alert("Hello " + name + ", Welcome to the " + course + " class!");
+
+// STEP_14
+alert("Hello " + name + ".\nWelcome to the JavaScript class!");
+
+// STEP_15
+let name = prompt("Enter your name:");
+alert("Hello " + name + ".\nWelcome to the JavaScript class!");
+
+// STEP_16
+let course = prompt("Enter your course:");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP_17
+let x = 10;
+let y = 20;
+window.console.log(x + y);
+
+// STEP_18
+let x = 20;
+window.console.log(x += 20);
+
+// STEP_19:
+let x = 20;
+window.console.log(x *= 20);
+
+// STEP_20:
+let x = 20%3;
+window.console.log(x/1);
+
+// STEP_21:
+let x = 0;
+window.console.log(x <= 14);
+
+// STEP_22:
+let x = 15;
+window.console.log(x === 16);
+
+// STEP_23:
+let widget = new Object();
+window.console.log(typeof widget);
+
+// STEP_24:
+let widget = new Object();
+window.console.log(widget instanceof Object);
+
+// STEP_25:
+window.console.log(widget instanceof Car);
