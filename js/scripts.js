6c48129347e7434fc3860137fc535439bf808792
Tiffany <tiffany.paulino@yahoo.com>
2020-02-06 18:12:57 -0800

    fisrt commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,95 @@
 // STEP 1
-var someMonth;
-function theMonth();
-var currentMonth;
-var summerMonth;
-var myLibraryFunction;
-
+var someMonth; 
+function theMonth(); 
+var currentMonth; 
+var summerMonth; 
+var myLibrayFunction();
 
 // STEP 2
-17 //numeric literal expression
-"Hello, World!" //string literal expression
-false //boolean literal expression
-null //null literal expression
+3.3
+"Hi!"
+true
+null 
 
 // STEP 3 
-let x = 45 * (17 - 12); // complex / variable expression
-let yx = x * (150 * 0.25); // complex / variable expression
+var anExpression = 2 (4/5) + 3; 
+var secondExpression = math.PI * radius * radius;
 
 // STEP 4 
-let sFirstName;
-let sLastName;
-let sAddress;
-let sCity;
-let sState;
-let nZipCode;
-let nYourAge;
-let sRefferalSource;
-let bMayWeContactYou;
+let firstName; 
+let lastName; 
+let yourAddress; 
+let yourCity; 
+let yourState; 
+let zipCode; 
+let yourAge; 
+let referralSource; 
+let mayWeContactYou; 
 
 // STEP 5
-let sFirstName = "Mariia", sLastName = "Okhota";
-
-let sCity;
-sCity = "Dnipro";
-
-let sState = "California";
-let nZipCode = 92115;
+firstName "Tiffany"; 
+lastName "Paulino"; 
+yourAddress "33 Nowhere Lane"; 
+yourCity "San Diego"; 
+yourState "California"; 
+zipCode "92103"; 
+yourAge "31"; 
+referralSource "Class"; 
+mayWeContactYou "No"; 
 
 // STEP 6
-let ticketPrice = 170;
-window.console.log("The Hawaii ticket price is " + ticketPrice + " dollars.");
+var x = 87; 
+window.console.log("Total hours required " + totalHours + " hours.");
 
 // STEP_7
 let x = false;
-window.console.log("Are we alone in the Universe? " + x);
+window.console.log("Are we living for freely? " + x);
 
-let y = 5
+let y = 15
 window.console.log(y + false);
 
 // STEP_8  fixed version
-var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+var someString = 'The most important thing is to try and inspire people so that they can be great in whatever they want to do';
 window.console.log(someString);
 
 // STEP_9
 let a = null;
 window.console.log(a);
 let b;
-window.console.log(Blob);
+window.console.log(Tiffany);
 
 // STEP_10
-window.console.log(typeof "Mariia");//string
-window.console.log(typeof 17);//number
-window.console.log(typeof true);//boolean
-window.console.log(typeof [1, 2, 3]);//object
-window.console.log(typeof a);//undefined
+window.console.log(typeof "Tiffany"); 
+window.console.log(typeof 23); 
+window.console.log(typeof true); 
+window.console.log(typeof [1, 2, 3]); 
+window.console.log(typeof a); 
 
 // STEP_11
-alert("Hello " + "Mariia Okhota" + ", Welcome to the JavaScript class!");
+alert("Hi " + "Tiffany Paulino" + ", Welcome to Disneyland!");
 
 // STEP_12
-let name = "Mariia Okhota";
-alert("Hello " + name + ", Welcome to the JavaScript class!");
+let name = "Tiffany Paulino";
+alert("Hi " + name + ", Welcome to Disneyland!");
 
 // STEP_13
 let course = "JavaScript";
-alert("Hello " + name + ", Welcome to the " + course + " class!");
+alert("Hi " + name + ", Welcome to  " + location + " Disneyland!");
 
 // STEP_14
-alert("Hello " + name + ".\nWelcome to the JavaScript class!");
+alert("Hi " + name + ".\nWelcome to Disneyland!");
 
 // STEP_15
 let name = prompt("Enter your name:");
-alert("Hello " + name + ".\nWelcome to the JavaScript class!");
+alert("Hi " + name + ".\nWelcome to Disneyland!");
 
 // STEP_16
-let course = prompt("Enter your course:");
-alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+let course = prompt("Enter location:");
+alert("Hi " + name + ".\nWelcome to " + location + " Disneyland!");
 
 // STEP_17
-let x = 10;
-let y = 20;
+let x = 20;
+let y = 55;
 window.console.log(x + y);
 
 // STEP_18
